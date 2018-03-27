9e968e62eac082244e20e6024adca76d80dba0a1
$J <backgroundnoise@outlook.com>
2018-03-27 11:06:44 -0400

    Add files via upload

---
+++ app/ACT_config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	     'shrine_url' => 'your_SHRINE_URL_here',
+	     'working_directory' => '/opt/viewer_jobs',
+	     'debug_logging' => true,
+             'shrine_connector_logging' => false,
+             'patient_list_exporter_logging' => false
+	     );
+?>
