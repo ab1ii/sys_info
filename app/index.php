2e1f94786b5574cd09cf97fa4dfd1bf2be8c5972
$J <backgroundnoise@outlook.com>
2018-03-27 15:02:51 -0400

    Add files via upload

--- app/index.php
+++ app/index.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <?php
-/*This is a series of tests to check  ACT plugins install.
-   --jmt67 2018
+/*This is a series of tests to check  
 */?>
 <html>
   <head> 
-    <title>ACT Validator</title>
+    <title>Sys Info</title>
     <link type="text/css" href="assets/ACT.css" rel="stylesheet" />
     <script src="js-ext/jquerycode/jquery-1.11.3.min.js"></script>
 
@@ -85,19 +84,8 @@
 
     </script>
   <section>
-  <h2>ACT Validator</h2>
-  <div style="position:relative;" >This ACT utility has checked your install for various requirements. If these requirements are not met, your ACT install may not run, or have issues. <br> 
-  The requirements checked are such things as, the correct version of PHP, correct PHP extensions, 
-  a working directory that has the correct permissions set, 
-  and more. As new requirements are created, their tests will be added to this page.  <br> 
-  By reviewing the test results below, one can easily see any potential issues with the ACT install.  <br>
-  Also you can click the <b>Refresh</b> button to see if changes you have made to your install have cleared up any issues. 
-  <br><br>
-  <a href="#" onclick="sendFeedback();"   title="Submit System Diagnostics."  role="button" style="display:none;float:right;" >Submit System Diagnostics</a>
-   <br>
-  </div>  
-  <div id="preCkDiv" style="display:none;" >For a system requirements test only, check the Prerequisites box.&nbsp;
-    <input id="preCheck" type="checkbox" name="preCheck" onclick="switchMode();" checked ><label for="preCheck"><b>Prerequisites</b></label>   
+  <h2>System Info</h2>  
+  <div id="preCkDiv" style="display:none;" >For a system requirements test only, check the Prerequisites box.&nbsp;      
     <a href="#" onclick="location.reload();"   title="Get latest test results."  role="button" style="float:right;" >Refresh</a>
   </div> 
   <div id="errDiv">PHP Error. Please Check ACT_config.php for errors.&nbsp;<a href="https://community.i2b2.org/wiki/display/ACT/Issues" target="_blank" title="Opens the ACT Plugin Wiki page for ACT config  issues in a new tab">more info</a> </div>
@@ -108,7 +96,7 @@
 
 require('ACT_inspector.php');
 
-$summary = '<section><div><h3>Results of ACT Testing</h3><ul>'; 
+$summary = '<section><div><h3>Results</h3><ul>'; 
 $styleClass='';
 
 //Hidden Info to be sent for future send feedback option
@@ -122,59 +110,29 @@ $results = new TestResults();
 $results = testACT_getOSInfo();
 $styleClass = setUIClass($results->fail,true);
 $rm = $results->message;
-$rm =  $rm . '&nbsp;' . $results->wiki->link();
+$rm =  $rm . '&nbsp;' ;
 $summary = $summary . '<li class="'.$styleClass.'">OS Info: ' .$rm . '</li>';
 
 $results = new TestResults();
 $results = testACT_getPHPVersion('php');
 $styleClass = setUIClass($results->fail);
 $rm = $results->message;
-$rm =  $rm . '&nbsp;' . $results->wiki->link();
+$rm =  $rm . '&nbsp;' ;
 $summary = $summary . '<li class="'.$styleClass.'">PHP Version: ' .$rm . '</li>';
 
 $results = new TestResults();
 $results = testACT_getPHPVersion('curl');
 $styleClass = setUIClass($results->fail);
 $rm = $results->message;
-$rm =  $rm . '&nbsp;' . $results->wiki->link();
+$rm =  $rm . '&nbsp;' ;
 $summary = $summary . '<li class="'.$styleClass.'">PHP cURL version: ' .$rm . '</li>';
 
 $results = new TestResults();
 $results = testACT_getPHPVersion('json');
 $styleClass = setUIClass($results->fail);
 $rm = $results->message;
-$rm =  $rm . '&nbsp;' . $results->wiki->link();
+$rm =  $rm . '&nbsp;' ;
 $summary = $summary . '<li class="'.$styleClass.'">PHP Extension JSON version: ' .$rm . '</li>';
-
-$results = new TestResults();
-$results = testACT_testSystem_command('at');
-$styleClass = setUIClass($results->fail);
-$rm = $results->message;
-$rm =  $rm . '&nbsp;(In addition, ATD service must be running.)&nbsp;' . $results->wiki->link();
-$summary = $summary . '<li class="'.$styleClass.'">AT command: ' .$rm . '</li>';
-
-//"POST" install checks
-//************************ */
-$results = new TestResults();
-$results = testACT_check_working_directory();
-$styleClass = setUIClass($results->fail);
-$rm = $results->message;
-$rm =  $rm . '&nbsp;' . $results->wiki->link();
-$summary = $summary .'<li style="display:none;" class="'.$styleClass.' preIn">Working Directory: ' . $rm . '</li>';
-
-$results = new TestResults();
-$results = testACT_getFreeSpace();
-$styleClass = setUIClass($results->fail);
-$rm = $results->message;
-$rm =  $rm . '&nbsp;' . $results->wiki->link();
-$summary = $summary . '<li style="display:none;" class="'.$styleClass.' preIn">Remaining Space: ' .$rm . '</li>';
-
-$results = new TestResults();
-$results = testACT_SHRINE_URL_found();
-$styleClass = setUIClass($results->fail);
-$rm = $results->message;
-$rm =  $rm . '&nbsp;' . $results->wiki->link();
-$summary = $summary .'<li style="display:none;" class="'.$styleClass.' preIn">SHRINE URL: ' . $rm . '</li>';
 
 
 
@@ -183,11 +141,8 @@ $summary = $summary . '</ui></div>';
 $results = new TestResults();
 $results = testACT_showACT_Config();
 $rm = $results->message;
-$rm =  $rm . '&nbsp;' . $results->wiki->link();
+$rm =  $rm . '&nbsp;' ;
 
-$togButton = '<a href="#" id="togButton" onclick="toggleConfigView();"   title="switch view of config file"  role="button" >Show</a>';
-
-$summary = $summary . '<div id="acDiv" style="display:none;" >View of ACT Config File&nbsp;' .$togButton . '<br>' . $rm .  '</div>';
 
 $summary = $summary . '</section>';
 
